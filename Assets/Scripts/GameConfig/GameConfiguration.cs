bde610cd152689ad56adb9b88719f3c66053b6de
Alberto <bertum_1993@hotmail.com>
2019-01-25 22:50:34 +0100

    Pig resources management

---
+++ Assets/Scripts/GameConfig/GameConfiguration.cs
@@ -0,0 +1,6 @@
+﻿public static class GameConfiguration
+{
+    public const int WHEATCAPACITY = 6;
+    public const int WOODCAPACITY = 3;
+    public const int STONECAPACITY = 2;
+}
---
+++ Assets/Scripts/Pig/PigController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PigController : MonoBehaviour
+{
+    private int wheat, wood, stone;
+    private Material currentMaterial;
+
+    private void Awake()
+    {
+        wheat = 0;
+        wood = 0;
+        stone = 0;
+    }
+
+    public bool UseMaterial()
+    {
+        bool used = false;
+        if (currentMaterial == Material.Wheat && wheat > 0)
+        {
+            // TODO reproduce use wheat sound
+            wheat--;
+            used = true;
+        }
+        else if (currentMaterial == Material.Wood && wood > 0)
+        {
+            // TODO reproduce use wood sound
+            wood--;
+            used = true;
+        }
+        else if (currentMaterial == Material.Stone && stone > 0)
+        {
+            // TODO reproduce use stone sound
+            stone--;
+            used = true;
+        }
+        if (!used)
+        {
+            // TODO sound pig without materials?
+        }
+        return used;
+    }
+
+    public void ChangeMaterial(Material newMaterial)
+    {
+        currentMaterial = newMaterial;
+        switch (currentMaterial)
+        {
+            case Material.Wheat:
+                // TODO reproduce get wheat sound
+                wheat = GameConfiguration.WHEATCAPACITY;
+                break;
+            case Material.Wood:
+                // TODO reproduce get wood sound
+                wood = GameConfiguration.WOODCAPACITY;
+                break;
+            case Material.Stone:
+                // TODO reproduce get stone sound
+                stone = GameConfiguration.STONECAPACITY;
+                break;
+        }
+    }
+}

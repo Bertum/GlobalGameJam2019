45bb398aac7f8ee5125448ec4c794e5594ba2e22
Alberto <bertum_1993@hotmail.com>
2019-01-26 10:23:25 +0100

    Game and Main Scenes, wolf alert WIP

---
+++ Assets/Scripts/Wolf/WolfAlert.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class WolfAlert : MonoBehaviour
+{
+    public AudioClip alertClip;
+    private AudioSource audioSource;
+
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = alertClip;
+    }
+
+    void Start()
+    {
+        audioSource.Play();
+    }
+}

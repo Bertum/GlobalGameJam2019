0b677178364bff2c450ee186fbc58f25276d1a50
Alberto <bertum_1993@hotmail.com>
2019-01-26 11:20:26 +0100

    Sprites and main menu WIP

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    private void LoadGame()
+    {
+        PlayerPrefs.Save();
+        SceneManager.LoadSceneAsync(GameConfiguration.GAMESCENE);
+    }
+
+    public void OnePlayer()
+    {
+        PlayerPrefs.SetInt("Players", 1);
+        LoadGame();
+    }
+
+    public void ThreePlayer()
+    {
+        PlayerPrefs.SetInt("Players", 3);
+        LoadGame();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowCredits()
+    {
+
+    }   //TODO show the credits
+}

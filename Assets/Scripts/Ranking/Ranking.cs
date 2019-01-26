fe709af73b952ec9956eac040319536c085cfa20
Alberto <bertum_1993@hotmail.com>
2019-01-26 17:08:42 +0100

    Ranking load, save and ranking panel

---
+++ Assets/Scripts/Ranking/Ranking.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+[Serializable]
+public class Ranking
+{
+    public string PlayerName { get; set; }
+    public float Time { get; set; }
+    public DateTime Date { get; set; }
+}
---
+++ Assets/Scripts/Ranking/RankingData.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class RankingData
+{
+    public List<Ranking> rankings = new List<Ranking>();
+}
